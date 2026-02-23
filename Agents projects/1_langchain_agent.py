7c3d8fa846be48487511a9213394049079753cd7
Harkirat <harkiratsingh20026@gmail.com>
2026-02-23 22:00:03 +0530

    Agent Project

--- Agents projects/1_langchain_agent.py
+++ Agents projects/1_langchain_agent.py
@@ -1,9 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.tools import tool, InjectedToolArg
 from langchain_community.tools import DuckDuckGoSearchRun
-from typing import Annotated
 from langchain_core.messages import HumanMessage, ToolMessage
-import requests
 from dotenv import load_dotenv
 
 load_dotenv()
